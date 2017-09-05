b43f5906535cd98b171723621081714e8a599620
Gints Murāns <gm@gm.lv>
2017-09-05 15:47:18 +0300

    Added ssh tunnel script

---
+++ Linux/ssh_tunnel.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Make sure you have screen installed
+# Debian:
+# apt-get install screen
+#
+# check process ssh-tunnel matching "^ssh -N sshtunnel"
+#    user root
+#    start program = "/root/ssh_tunnel.sh"
+#    if does not exist then start
+#
+# See ~/.ssh/config for ssh-tunel
+# Example:
+# nano ~/.ssh/config
+# Host sshtunnel
+#      HostName [hostname]
+#      User root
+#      ServerAliveInterval 15
+#      ServerAliveCountMax 3
+#
+#      RemoteForward 0.0.0.0:2201 localhost:22
+#
+
+
+screen -dmS ssh-tunnel ssh -N sshtunnel
